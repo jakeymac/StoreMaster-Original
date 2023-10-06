9cce4735c2dfa86189b6529518de6d242de6474e
Jacob Johnson <jakeymacaroni@gmail.com>
2023-10-05 23:42:00 -0600

    Removing Files to Reset

--- StoreMaster/Purchases/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "Purchases"
-
-urlpatterns = [
-    path('purchases', views.index,name='index'),
-]
--- StoreMaster/Shipments/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Shipments Home")
